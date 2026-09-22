5e075bc131d7e1530535cc10d6930064e93e6f42
Cemre <zdemirhan.02@gmail.com>
2026-09-22 11:16:38 +0300

    refactor: remove mandatory JWT auth, configure centralized CORS, and fix package structures

--- src/main/java/inventory_management/controller/CategoryController.java
+++ src/main/java/inventory_management/controller/CategoryController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     private final CategoryService categoryService;
